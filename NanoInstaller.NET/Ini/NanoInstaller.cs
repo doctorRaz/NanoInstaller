0a65e89b5def52cb0796263756c2bd44cb42d3de
doctorRaz <36532296+doctorRaz@users.noreply.github.com>
2025-03-21 10:14:38 +0300

    Init
    пока собирается

---
+++ NanoInstaller.NET/Ini/NanoInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drz.NanoInstallerFromIni.Ini
+{
+    internal class NanoInstaller
+    {
+
+
+    }
+}
